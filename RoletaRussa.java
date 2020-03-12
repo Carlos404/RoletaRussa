cfc65aca59d46ed1a423869e241e4160ae96ac83
Carlos404 <carlosdev@outlook.com.br>
2020-03-12 13:47:34 -0300

    Inserindo validacao de dados

--- RoletaRussa.java
+++ RoletaRussa.java
@@ -1,29 +1,57 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class RoletaRussa {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		Scanner scanner = new Scanner(System.in);
+
+		// Quantidade de jogadores
+		System.out.println("Quantidade de jogadores");
+		int qtdJogadores = Integer.parseInt(scanner.nextLine());
+		System.out.println("------------------------------------------------------");
+
+		// Nome dos Jogadores
+		ArrayList<String> jogadores = new ArrayList<String>();
+		System.out.println("Digite o nome dos Jogadores");
+		for (int q = 1; q <= qtdJogadores;) {
+			System.out.println("Jogador: " + q);
+			String nomeJogadores = scanner.nextLine();
+			jogadores.add(nomeJogadores);
+			q++;
+		}
+		System.out.println("------------------------------------------------------");
+
+		// Quantidade de tiros
+		System.out.println("Quantidade de tentativas");
+		int qtdTiros = Integer.parseInt(scanner.nextLine());
+		if(qtdTiros <= 0) {
+			System.out.println("Quantidade de tentativas deve ser superior a 0");
+		}
+		else {
+			System.out.println("------------------------------------------------------");
+		}
 		
-		int tiros = 6;
+		int random = (int) (Math.random() * qtdTiros + 1);
+		int numero = 0;
+		for (int i = 1; i <= qtdTiros;) {
 
-		int random = (int) (Math.random() * 6 + 1);
-
-		for (int i = 1; i <= tiros;) {
-			System.out.println("Digite atirar para jogar");
+			System.out.println(jogadores.get(numero) + " Digite atirar para jogar");
 			String atirar = scanner.nextLine().toLowerCase();
-
 			if (i != random && atirar.equals("atirar")) {
 				System.out.println("Essa foi por pouco");
 				i++;
+				numero++;
+				if (numero >= qtdJogadores) {
+					numero = 0;
+				}
 			} else if (atirar != "atirar" && random != i) {
 				System.out.println("Você precisa digitar ATIRAR");
 			} else if (i == random) {
-				System.out.println("Você morreu");
+				System.out.println(jogadores.get(numero) + " Morreu");
 				break;
 			}
 		}
-
 		scanner.close();
 	}
 }
